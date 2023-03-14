8d6e8ee64893151fb71c9bbc6025d3c7ad299b9a
Ces Espiritu <mariacecilia.espiritu@itechsystemsolutions.com>
2023-03-14 10:23:03 +0800

    addtn'l updates

--- resources/views/layouts/webapp.blade.php
+++ resources/views/layouts/webapp.blade.php
@@ -75,7 +75,7 @@
          <div class="desktop-nav">
             <div class="container">
                <nav class="navbar navbar-expand-md navbar-light">
-                  <a class="navbar-brand" href="index-3.html">
+                  <a class="navbar-brand" href="/">
                   <img src="assets/images/logo.png" class="main-logo" alt="logo">
                   </a>
                   <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent" style="display: block;">
