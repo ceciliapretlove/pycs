2ccdc8226556efedc398db21a8f3ba7a1a058968
Ces Espiritu <mariacecilia.espiritu@itechsystemsolutions.com>
2023-03-17 22:17:22 +0800

    robots and analytics

--- resources/views/layouts/webapp.blade.php
+++ resources/views/layouts/webapp.blade.php
@@ -4,6 +4,7 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="robots" content="noindex">
       <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
       <meta name="google-site-verification" content="SQqOhMZxpoBHNw3we6yhBu3kl67GHK5xfpr7omJoNrk" />
       <link rel="stylesheet" href="./assets/css/meanmenu.css">
