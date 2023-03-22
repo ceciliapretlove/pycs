ede44aac9a3598a9030ab1fa78910d9b54d6fff2
Ces Espiritu <mariacecilia.espiritu@itechsystemsolutions.com>
2023-03-22 14:45:35 +0800

    addtn'l updates

--- resources/views/website-pages/humble-begginings-of-pycs.blade.php
+++ resources/views/website-pages/humble-begginings-of-pycs.blade.php
@@ -64,77 +64,7 @@
                         SCHOOL</strong>. The law further required that members of the Board of Trustees, as well as
                         school officials, must be Filipino citizens.
                      </p>
-                     <div class="submit">
-                        <h3>Philosophy</h3>
-                        <p>Philippine Yuh Chiau School, Inc. is primarily a <em>“Filipinized”</em> Chinese school. It believes in the
-                           holistic development of its learners through its dual curricula- an English curriculum which
-                           provides balance learning experiences infused with positive values and principles that prepare its
-                           learners for real-life challenges and be globally competent; and a Chinese curriculum which
-                           includes simplified Chinese language arts which serves as catalyst for a harmonious relationship
-                           among the members of the Filipino-Chinese community.
-                        </p>
-                        <h3>Vision Statement</h3>
-                        <p>Philippine Yuh Chiau School, Inc. envisions its learners to be imbued with Confucian virtues,
-                           skills, and functional literacy at par with global standards to face the challenges of life ahead.
-                        </p>
-                        <h3>Mission Statement</h3>
-                        <p>Philippine Yuh Chiau School, Inc. provides academic excellence through the integrated English
-                           and Chinese curricula; advocates deep love of country; promotes Filipino-Chinese friendship and
-                           unity; and contributes in the attainment of local, national, and global progress.
-                        </p>
-                        <h3>Core Values</h3>
-                        <p>A 21 st Century education is about giving learners the skills they need to succeed in this new
-                           complex world, and helping them grow the confidence to put into practice those skills. With so
-                           much information readily available to them, Philippine Yuh Chiau School, Inc. cannot afford to
-                           merely meeting the challenges of the times leaving behind its Core Values.
-                        </p>
-                        <p>The Philippine Yuh Chiau School strongly believes that every Yuh Chiauian will genuinely be
-                           considered a local, national, and global pride if he is imbued with the following CORE VALUES
-                           and skill:
-                        </p>
-                        <div class="apply-list" style="border-bottom: none; padding: 0;">
-                           <div class="row">
-                              <div class="col-lg-6 col-md-6">
-                                 <ul>
-                                    <li><i class="ri-checkbox-circle-line"></i>God Loving</li>
-                                    <li><i class="ri-checkbox-circle-line"></i>Respectful To Environment</li>
-                                    <li><i class="ri-checkbox-circle-line"></i>Humane</li>
-                                    <li><i class="ri-checkbox-circle-line"></i>Self-disciplined</li>
-                                    <li><i class="ri-checkbox-circle-line"></i>Critical Thinker</li>
-                                 </ul>
-                              </div>
-                           </div>
-                        </div>
-                        <h3>Confucian Virtues</h3>
-                        <p>A full-pledged and true YUH CHIAUIAN is believed to possess the following traits in
-                           promoting individual and collective greatness:
-                        </p>
-                        <ul class="confucian-list">
-                           <li><span>仁 Ren</span> - Benevolence, Charity, and Humanity</li>
-                           <li><span>义 Yi</span> - Honesty and Uprightness</li>
-                           <li><span>智 Zhi</span> - Knowledge</li>
-                           <li><span>信 Xin</span> - Faithfulness and Integrity</li>
-                           <li><span>礼 Li</span> - Correct Behavior and Worship</li>
-                        </ul>
-                        <p></p>
-                        <h3>Institutional Objectives</h3>
-                        <p>The two core objectives of Philippine Yuh Chiau School, Inc. are: <em>Excellence in Discipline and
-                           Excellence in Academics</em>, thus, every Yuh Chiauian is expected to:
-                        </p>
-                        <div class="apply-list" style="border-bottom: none; padding: 0;">
-                           <ul>
-                              <li><i class="ri-checkbox-circle-line"></i>show sensitivity in respecting the rights of others;</li>
-                              <li><i class="ri-checkbox-circle-line"></i>practice habitual action of willfully choosing what is right and acting with prudence;</li>
-                              <li><i class="ri-checkbox-circle-line"></i>demonstrate acts of kindness, generosity, compassion, and love to the country;</li>
-                              <li><i class="ri-checkbox-circle-line"></i>show active environmental awareness through conservation and preservation of naturalresources for sustainable development;</li>
-                              <li><i class="ri-checkbox-circle-line"></i>foster deep love of country through civic engagement;</li>
-                              <li><i class="ri-checkbox-circle-line"></i>show cultural awareness through active participation in different activities that expose Filipino-Chinese values, traditions, and practices;</li>
-                              <li><i class="ri-checkbox-circle-line"></i>express thoughts both in oral and written expressions effectively;</li>
-                              <li><i class="ri-checkbox-circle-line"></i>demonstrate the 21 st Century skills</li>
-                              <li><i class="ri-checkbox-circle-line"></i>establish a personal motivation to become a local, national, and global pride of thecountry.</li>
-                           </ul>
-                        </div>
-                     </div>
+
                      <div class="academics-area pt-50 pb-70">
                         <div class="row">
                            <h3 class="ptb-50">Founders of Philippine Yuh Chiau School</h3>
